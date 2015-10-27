cf0ba98471f61ae8597d700d235e3f7b2b83e61d
Mark Bourke <markkbourke@gmail.com>
2015-10-27 19:31:24 +0000

    Follow Button

---
+++ Tomahawk/NowPlayingViewController.h
@@ -0,0 +1,13 @@
+//
+//  NowPlayingViewController.h
+//  Tomahawk
+//
+//  Created by Mark Bourke on 26/10/2015.
+//  Copyright © 2015 Mark Bourke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NowPlayingViewController : UIViewController
+
+@end
---
+++ Tomahawk/NowPlayingViewController.m
@@ -0,0 +1,37 @@
+//
+//  NowPlayingViewController.m
+//  Tomahawk
+//
+//  Created by Mark Bourke on 26/10/2015.
+//  Copyright © 2015 Mark Bourke. All rights reserved.
+//
+
+#import "NowPlayingViewController.h"
+
+@interface NowPlayingViewController ()
+
+@end
+
+@implementation NowPlayingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
